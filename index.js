5fec6931cfcf1f26003fe4e1adf77fae64aba606
Andrew Suares <andrewlsuares@gmail.com>
2021-06-02 21:07:55 -0700

    more changes

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let firstCard = 2
+// Global Scope Variables
+let firstCard = 10
 let secondCard = 11
+const sum = firstCard + secondCard
+let message = ''
+
+// State of the start of Game
+let hasBlackjack = false
+let isAlive = true
+
+// Logic of the game
+if (sum <= 20) {
+	message = 'Do you want a card'
+} else if (sum === 21) {
+	message = 'You have won the game'
+	hasBlackjack = true
+} else {
+	message = 'you busted out'
+	isAlive = false
+}
+
+console.log(message)
